146f52b0da55f912ed5d3cc04a9a2ba9f0892423
Yeboah Godfred <ygodfred3@gmail.com>
2022-12-12 13:06:13 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Return: Always 0 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
